0dac440dae7ca40a8d1be51878c00599272a6f2d
Jeremías <jimmyguionbajo@gmail.com>
2020-05-28 17:44:50 -0400

    FIX AXIOS

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 export default function ({ $axios, app, store }) {
   $axios.onRequest(config => {
-    config.baseURL = 'https://flip-book-api.herokuapp.com/api/v1/';
+    config.baseURL = 'https://flip-book-api.herokuapp.com/api/v1';
   })
 }
